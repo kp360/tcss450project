46853ce4c4d4dcb0d73c79732d2858f480292e19
Kyle Phan <knaph@uw.edu>
2017-01-31 13:28:39 -0800

    Initial Commit for Just Jokes repo

---
+++ app/src/main/java/tcss450team3/uw/tacoma/edu/justjokes/MainActivity.java
@@ -0,0 +1,13 @@
+package tcss450team3.uw.tacoma.edu.justjokes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
