523d92c0514065353de678493e85e9e149ca8d9c
vk360class <class360github@gmail.com>
2017-02-11 15:07:53 -0800

    Added login stuff.
    Login text fields and submit button. Clicking submit opens the JokesPage activity.

--- app/src/main/java/tcss450team3/uw/tacoma/edu/justjokes/JokesPage.java
+++ app/src/main/java/tcss450team3/uw/tacoma/edu/justjokes/JokesPage.java
@@ -4,17 +4,13 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class JokesPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    public void login(View view) {
-        Intent intent = new Intent(this, JokesPage.class);
-        startActivity(intent);
+        setContentView(R.layout.activity_jokes_page);
     }
 }
