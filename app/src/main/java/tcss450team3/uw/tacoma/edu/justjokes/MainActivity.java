7e2fce37e6d046b1e59620fbc7f4379937708691
kp360 <knaph@uw.edu>
2017-02-17 18:55:22 -0800

    Commented MainActivity

--- app/src/main/java/tcss450team3/uw/tacoma/edu/justjokes/MainActivity.java
+++ app/src/main/java/tcss450team3/uw/tacoma/edu/justjokes/MainActivity.java
@@ -25,6 +25,7 @@ import java.net.URLEncoder;
  *  The purpose of this activity is for the user to log in to an existing
  *  account in our database, or to register and add a user to the database.
  *
+ *  @author Kyle Phan 2/16/17
  */
 public class MainActivity extends AppCompatActivity {
 
@@ -37,11 +38,14 @@ public class MainActivity extends AppCompatActivity {
     private EditText mUserUsernameEditText;
     private EditText mUserPasswordEditText;
 
-    private String USERNAME = "userName";
-    private String PASSWORD = "passWord";
-
-    private boolean loggedIn = false;
-
+    /**
+     * Method called when this Activity is created.
+     *
+     * Used to add OnClickListeners to the Login and Register buttons mapped to their
+     * respective Async Tasks to retrieve/send information to the database.
+     *
+     * @param savedInstanceState Stores data that was sent from the caller.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -71,17 +75,42 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    /**
+     * Method called when the Submit button is pressed.
+     *
+     * Initiates the AsyncTask that checks if the username and password entered
+     * by the user is in our database, logs in if it exists.
+     *
+     * @param url a String containing a url query for our database
+     */
     public void login(String url) {
 
         DownloadUsersTask task = new DownloadUsersTask();
         task.execute(new String[]{url.toString()});
     }
 
+    /**
+     * Method called when the Register button is pressed.
+     *
+     * Initiates the AsyncTask that checks if the username and password entered
+     * by the user exists in our database, creates new entry if it doesn't.
+     *
+     * @param url a String containing a url query for our database
+     */
     public void register(String url) {
         RegisUserTask task = new RegisUserTask();
         task.execute(new String[]{url.toString()});
     }
 
+    /**
+     * Method used to build a string used to query our database.
+     *
+     * This method concatenates and encodes the user entered information with the stored
+     * base URL into a string.
+     * @param v the current app view, used to display a Toast
+     * @param url String url containing the base URL
+     * @return the concatenated String to be used to query the database.
+     */
     private String buildURL(View v, String url) {
 
         StringBuilder sb = new StringBuilder(url);
@@ -108,8 +137,20 @@ public class MainActivity extends AppCompatActivity {
         return sb.toString();
     }
 
+    /**
+     * This inner class is used to connect to the database and check if the user entered
+     * username and password exist in our database, if they exist they're logged in
+     * if they don't exist, the login fails.
+     */
     private class DownloadUsersTask extends AsyncTask<String, Void, String> {
 
+        /**
+         * Accesses the server via the passed URL and performs the query
+         * if there is an issue, throws exception, if not passes on to onPostExecute.
+         *
+         * @param urls string URLs used to query the database
+         * @return String response from server
+         */
         @Override
         protected String doInBackground(String... urls) {
             String response = "";
@@ -128,7 +169,7 @@ public class MainActivity extends AppCompatActivity {
                     }
 
                 } catch (Exception e) {
-                    response = "Unable to download the list of users, Reason: "
+                    response = "Unable to download users, Reason: "
                             + e.getMessage();
                 } finally {
                     if (urlConnection != null)
@@ -138,6 +179,16 @@ public class MainActivity extends AppCompatActivity {
             return response;
         }
 
+        /**
+         * Method to respond after doInBackground.
+         *
+         * If server returns success then Jokes List is displayed and number of pages is
+         * calculated.
+         *
+         * If server returns anything else then toast displays what error the server returned.
+         *
+         * @param result String response from doInBackground containing the server's response
+         */
         @Override
         protected void onPostExecute(String result) {
             try {
@@ -147,8 +198,10 @@ public class MainActivity extends AppCompatActivity {
                     Toast.makeText(getApplicationContext(), "User logged in!"
                             , Toast.LENGTH_LONG)
                             .show();
+
                     double totalNumOfJokes = Double.parseDouble((String)jsonObject.get("numJokes"));
                     int numPagesOfJokes = (int) Math.ceil(totalNumOfJokes/NUM_JOKES_PER_PAGE);
+
                     Intent intent = new Intent(getApplicationContext(), JokesPage.class);
                     intent.putExtra("numPages", numPagesOfJokes);
                     startActivity(intent);
@@ -166,8 +219,20 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * This inner class is used to connect to the database and check if the user entered
+     * username and password exist in our database, if they don't exist they're registered
+     * if they do exist, the registration fails.
+     */
     private class RegisUserTask extends AsyncTask<String, Void, String> {
 
+        /**
+         * Accesses the server via the passed URL and performs the query
+         * if there is an issue, throws exception, if not passes on to onPostExecute.
+         *
+         * @param urls string URLs used to query the database
+         * @return String response from server
+         */
         @Override
         protected String doInBackground(String... urls) {
             String response = "";
@@ -199,11 +264,13 @@ public class MainActivity extends AppCompatActivity {
 
 
         /**
-         * It checks to see if there was a problem with the URL(Network) which is when an
-         * exception is caught. It tries to call the parse Method and checks to see if it was successful.
-         * If not, it displays the exception.
+         * Method to respond after doInBackground.
          *
-         * @param result
+         * If server returns success then user is registered and they can now sign in.
+         *
+         * If server returns anything else then toast displays what error the server returned.
+         *
+         * @param result String response from doInBackground containing the server's response
          */
         @Override
         protected void onPostExecute(String result) {
