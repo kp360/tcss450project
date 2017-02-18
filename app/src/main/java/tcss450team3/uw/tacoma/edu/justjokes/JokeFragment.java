kp360/tcss450project

commit ffd1c2340880daca62f5573465e576297baa5ce4

Clean up, clean up.
Edited comments, added a few, deleted a couple extra methods.

app/src/main/java/tcss450team3/uw/tacoma/edu/justjokes/JokeFragment.java
@@ -30,15 +30,29 @@ import java.util.List;
  * @author Vlad (2.15.17)
  */
 public class JokeFragment extends Fragment {
+
+    /** The URL to the php file that handles joke retrieval. */
     private static final String COURSE_URL
-            = "http://cssgate.insttech.washington.edu/~_450bteam3/list.php?cmd=";
+            = "http://cssgate.insttech.washington.edu/~_450bteam3/list.php?page=";
+
+    /** A RecylerView object that handles smooth list scrolling. */
     private RecyclerView mRecyclerView;
+
+    /** This variable holds the amount of pages of jokes our database currently has. It is used to
+     * disable the next button, so the user doesn't encounter any pages without jokes. */
     private int mNumPages = 1;
+
+    /** This variable holds the current page that the user is viewing, it is used to ensure that the
+     * previous (prev) and next buttons are enabled/disabled at the proper times. */
     private int mCurrentPageNum = 1;
 
+    /** Auto-generated variable. */
     private static final String ARG_COLUMN_COUNT = "column-count";
 
+    /** Auto-generated variable. */
     private int mColumnCount = 1;
+
+    /** Auto-generated variable, used to detect user interaction. */
     private OnListFragmentInteractionListener mListener;
 
     /**
@@ -48,7 +62,12 @@ public class JokeFragment extends Fragment {
     public JokeFragment() {
     }
 
-
+    /**
+     * Auto-generated constructor, not modified by us.
+     *
+     * @param columnCount Auto-generated variable.
+     * @return Returns a JokeFragment object.
+     */
     @SuppressWarnings("unused")
     public static JokeFragment newInstance(int columnCount) {
         JokeFragment fragment = new JokeFragment();
@@ -76,7 +95,7 @@ public class JokeFragment extends Fragment {
     }
 
     /**
-     * This method downloads the list of jokes, and also provides functionality for the
+     * This method downloads the page of jokes, and also provides functionality for the
      * 'Next'/'Prev' buttons.
      *
      * @param inflater A LayoutInflater object, that is used to get a View.
@@ -106,14 +125,16 @@ public class JokeFragment extends Fragment {
         final Button nextButton = (Button) getActivity().findViewById(R.id.nextButton);
 
         //Decrements the current page number variable and loads the jokes from that page.
-        // Enables/disables buttons accordingly.
+        //Enables/disables buttons accordingly.
         prevButton.setOnClickListener(new Button.OnClickListener() {
             public void onClick(View v) {
                 mCurrentPageNum--;
-                if (mCurrentPageNum == 1) {
+                if (mCurrentPageNum == 1)
                     prevButton.setEnabled(false);
-                }
-                nextButton.setEnabled(true);
+
+                if (!nextButton.isEnabled())
+                    nextButton.setEnabled(true);
+
                 new DownloadCoursesTask().execute(new String[]{COURSE_URL + mCurrentPageNum});
             }
         });
@@ -143,31 +164,6 @@ public class JokeFragment extends Fragment {
     }
 
     /**
-     * Auto-generated method, not modified by us.
-     *
-     * @param context A Context object.
-     */
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnListFragmentInteractionListener) {
-            mListener = (OnListFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnListFragmentInteractionListener");
-        }
-    }
-
-    /**
-     * Auto-generated method, not modified by us.
-     */
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
      * This interface must be implemented by activities that contain this
      * fragment to allow an interaction in this fragment to be communicated
      * to the activity and potentially other fragments contained in that
@@ -189,7 +185,7 @@ public class JokeFragment extends Fragment {
     private class DownloadCoursesTask extends AsyncTask<String, Void, String> {
 
         /**
-         * Accesses the server, and retrieves the page's source.
+         * Accesses the server, and retrieves the page's data.
          *
          * @param urls URLs to access.
          * @return Returns the webpage's source.
@@ -225,7 +221,7 @@ public class JokeFragment extends Fragment {
         /**
          * Notifies the user of what happened via Toast, and updates the list if no issues occurred.
          *
-         * @param result The server's response/webpage's source.
+         * @param result An error message, or the server's response/webpage's data.
          */
         @Override
         protected void onPostExecute(String result) {

app/src/main/java/tcss450team3/uw/tacoma/edu/justjokes/CustomJokeDialogFragment.java
@@ -41,9 +41,7 @@ public class CustomJokeDialogFragment extends DialogFragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    private String mParam1;
-    private String mParam2;
-
+    /** Auto-generated variable, used to detect user interaction. */
     private OnFragmentInteractionListener mListener;
 
     public CustomJokeDialogFragment() {
@@ -77,10 +75,6 @@ public class CustomJokeDialogFragment extends DialogFragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     /**
@@ -135,12 +129,6 @@ public class CustomJokeDialogFragment extends DialogFragment {
         }
     }
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
     /**
      * This interface must be implemented by activities that contain this
      * fragment to allow an interaction in this fragment to be communicated
@@ -150,6 +138,8 @@ public class CustomJokeDialogFragment extends DialogFragment {
      * See the Android Training lesson <a href=
      * "http://developer.android.com/training/basics/fragments/communicating.html"
      * >Communicating with Other Fragments</a> for more information.
+     *
+     * Auto-generated method, not modified by us.
      */
     public interface OnFragmentInteractionListener {
         void onFragmentInteraction(Uri uri);

app/src/main/java/tcss450team3/uw/tacoma/edu/justjokes/JokesPage.java
@@ -18,7 +18,7 @@ public class JokesPage extends AppCompatActivity implements JokeFragment.OnListF
     /**
      * This method handles opening a CustomJokeDialogFragment when a joke in the list is tapped on.
      *
-     * @param joke The Joke that whose values (setup, punchline) will be displayed in the
+     * @param joke The Joke whose values (setup, punchline) will be displayed in the
      *             CustomJokeDialogFragment.
      */
     @Override
@@ -33,7 +33,7 @@ public class JokesPage extends AppCompatActivity implements JokeFragment.OnListF
 
     /**
      * This method is called when the JokesPage Activity is created. It opens a JokeFragment object,
-     * and passes it the number of pages of jokes are currently in our database.
+     * and passes it the number of pages of jokes that are currently in our database.
      *
      * @param savedInstanceState Stores data that was sent from the caller.
      */

app/src/main/java/tcss450team3/uw/tacoma/edu/justjokes/MainActivity.java
@@ -29,13 +29,21 @@ import java.net.URLEncoder;
  */
 public class MainActivity extends AppCompatActivity {
 
+    /** The URL to the php file that handles user logins. */
     private static final String LOGIN_URL
             = "http://cssgate.insttech.washington.edu/~_450bteam3/login.php?";
+
+    /** The URL to the php file that handles user registration. */
     private static final String REGIS_URL
             = "http://cssgate.insttech.washington.edu/~_450bteam3/adduser.php?";
+
+    /** The amount of jokes that are displayed on each page. */
     private static final int NUM_JOKES_PER_PAGE = 20;
 
+    /** The EditText where users type their username. */
     private EditText mUserUsernameEditText;
+
+    /** The EditText where users type their password. */
     private EditText mUserPasswordEditText;
 
     /**
@@ -58,7 +66,6 @@ public class MainActivity extends AppCompatActivity {
         userLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                String url = buildLoginURL(v);
                 String url = buildURL(v, LOGIN_URL);
                 login(url);
             }
@@ -68,7 +75,6 @@ public class MainActivity extends AppCompatActivity {
         userRegisButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                String url = buildRegisURL(v);
                 String url = buildURL(v, REGIS_URL);
                 register(url);
             }
@@ -84,7 +90,6 @@ public class MainActivity extends AppCompatActivity {
      * @param url a String containing a url query for our database
      */
     public void login(String url) {
-
         DownloadUsersTask task = new DownloadUsersTask();
         task.execute(new String[]{url.toString()});
     }

commit 75ba9348c2139fe461e162774d41f2770d439447

Added a TextView to show the user what page their currently on. Fixed a bug introduced in commit: ffd1c23
Deleted the onAttach method in commit ffd1c2, which broke the joke popup dialog. Re-added it in this commit, and now the joke pops up again. Added the page num TextView, which shows the user what page their currently looking at.

app/src/main/java/tcss450team3/uw/tacoma/edu/justjokes/JokeFragment.java
@@ -12,6 +12,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import tcss450team3.uw.tacoma.edu.justjokes.joke.Joke;
@@ -35,8 +36,14 @@ public class JokeFragment extends Fragment {
     private static final String COURSE_URL
             = "http://cssgate.insttech.washington.edu/~_450bteam3/list.php?page=";
 
+    /** This text is used to convey to the user what page they're currently on. */
+    private static final String PAGE_TEXT = " Page: ";
+
     /** A RecylerView object that handles smooth list scrolling. */
     private RecyclerView mRecyclerView;
+
+    /** The TextView that tells the user what page of jokes they are currently browsing. */
+    private TextView mPageNumTextView;
 
     /** This variable holds the amount of pages of jokes our database currently has. It is used to
      * disable the next button, so the user doesn't encounter any pages without jokes. */
@@ -91,7 +98,6 @@ public class JokeFragment extends Fragment {
         if (args != null) {
             mNumPages = args.getInt("numPages");
         }
-
     }
 
     /**
@@ -117,7 +123,7 @@ public class JokeFragment extends Fragment {
             } else {
                 mRecyclerView.setLayoutManager(new GridLayoutManager(context, mColumnCount));
             }
-            DownloadCoursesTask task = new DownloadCoursesTask();
+            DownloadJokesTask task = new DownloadJokesTask();
             task.execute(new String[]{COURSE_URL + mCurrentPageNum});
         }
 
@@ -135,7 +141,8 @@ public class JokeFragment extends Fragment {
                 if (!nextButton.isEnabled())
                     nextButton.setEnabled(true);
 
-                new DownloadCoursesTask().execute(new String[]{COURSE_URL + mCurrentPageNum});
+                new DownloadJokesTask().execute(new String[]{COURSE_URL + mCurrentPageNum});
+                updatePageNumTextView();
             }
         });
 
@@ -150,7 +157,8 @@ public class JokeFragment extends Fragment {
                 if (!prevButton.isEnabled())
                     prevButton.setEnabled(true);
 
-                new DownloadCoursesTask().execute(new String[]{COURSE_URL + mCurrentPageNum});
+                new DownloadJokesTask().execute(new String[]{COURSE_URL + mCurrentPageNum});
+                updatePageNumTextView();
             }
         });
 
@@ -160,7 +168,36 @@ public class JokeFragment extends Fragment {
         if (mCurrentPageNum == mNumPages)
             nextButton.setEnabled(false);
 
+        mPageNumTextView = (TextView) getActivity().findViewById(R.id.pageNum);
+        updatePageNumTextView();
+
         return view;
+    }
+
+    /**
+     * This method is used to update the TextView that shows the current page number.
+     */
+    private void updatePageNumTextView() {
+        mPageNumTextView.setText(PAGE_TEXT + mCurrentPageNum + " ");
+    }
+
+    /**
+     * Auto-generated method, not modified by us.
+     *
+     * This method is critical because it initializes mListener, which detects when the user
+     * interacts with our joke list.
+     *
+     * @param context A Context object.
+     */
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnListFragmentInteractionListener) {
+            mListener = (OnListFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnListFragmentInteractionListener");
+        }
     }
 
     /**
@@ -182,7 +219,7 @@ public class JokeFragment extends Fragment {
     /**
      * This class is used to retrieve the current page of jokes, and list them in the fragment.
      */
-    private class DownloadCoursesTask extends AsyncTask<String, Void, String> {
+    private class DownloadJokesTask extends AsyncTask<String, Void, String> {
 
         /**
          * Accesses the server, and retrieves the page's data.

app/src/main/res/layout/activity_jokes_page.xml
@@ -20,25 +20,34 @@
         android:layout_height="wrap_content"
         android:orientation="horizontal">
         <TableRow>
-    <Button
-        android:id="@+id/prevButton"
-        android:layout_width="wrap_content"
-        android:layout_height="wrap_content"
-        android:textAllCaps="false"
-        android:text="Prev"
-        android:minHeight="0dp"
-        android:minWidth="0dp"
-        />
+            <Button
+                android:id="@+id/prevButton"
+                android:layout_width="wrap_content"
+                android:layout_height="wrap_content"
+                android:textAllCaps="false"
+                android:text="@string/previous"
+                android:minHeight="0dp"
+                android:minWidth="0dp"
+                />
 
-    <Button
-        android:id="@+id/nextButton"
-        android:layout_width="wrap_content"
-        android:layout_height="wrap_content"
-        android:textAllCaps="false"
-        android:text="Next"
-        android:minHeight="0dp"
-        android:minWidth="0dp"
-        />
+            <TextView
+                android:id="@+id/pageNum"
+                android:layout_width="wrap_content"
+                android:layout_height="wrap_content"
+                android:textAllCaps="false"
+                android:minHeight="0dp"
+                android:minWidth="0dp"
+                />
+
+            <Button
+                android:id="@+id/nextButton"
+                android:layout_width="wrap_content"
+                android:layout_height="wrap_content"
+                android:textAllCaps="false"
+                android:text="@string/next"
+                android:minHeight="0dp"
+                android:minWidth="0dp"
+                />
         </TableRow>
     </TableLayout>
 </LinearLayout>

app/src/main/res/values/strings.xml
@@ -5,7 +5,7 @@
     <string name="password">Password</string>
     <string name="submit">Submit</string>
     <string name="register">Register</string>
-    <string name="bypassLogin">Bypass Login</string>
     <string name="showPunchline">Show Punchline</string>
-
+    <string name="previous">Prev</string>
+    <string name="next">Next</string>
 </resources>