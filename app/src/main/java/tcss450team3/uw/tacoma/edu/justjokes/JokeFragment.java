45d0cbc0391de16c8a12d73fb9e57069c888b27d
vk450class <class450github@gmail.com>
2017-02-17 22:15:50 -0800

    Added onDetach method
    Added because appears to make navigation smoother...maybe not...anyhow it doesn't seem to hurt.

--- app/src/main/java/tcss450team3/uw/tacoma/edu/justjokes/JokeFragment.java
+++ app/src/main/java/tcss450team3/uw/tacoma/edu/justjokes/JokeFragment.java
@@ -201,6 +201,15 @@ public class JokeFragment extends Fragment {
     }
 
     /**
+     * Auto-generated method, not modified by us.
+     */
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
      * This interface must be implemented by activities that contain this
      * fragment to allow an interaction in this fragment to be communicated
      * to the activity and potentially other fragments contained in that
